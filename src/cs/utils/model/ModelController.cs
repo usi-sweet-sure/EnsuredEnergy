94fc6183888aee06d1cca458b1fe585d19059d8e
Andrew Dobis <andrew.dobis@epfl.ch>
2023-07-28 17:54:32 +0200

    License comment for modelcontroller

--- src/cs/utils/model/ModelController.cs
+++ src/cs/utils/model/ModelController.cs
@@ -1,3 +1,20 @@
+/**
+	Sustainable Energy Development game modeling the Swiss energy Grid.
+	Copyright (C) 2023 Università della Svizzera Italiana
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using Godot;
 using System;
 using System.Xml;
