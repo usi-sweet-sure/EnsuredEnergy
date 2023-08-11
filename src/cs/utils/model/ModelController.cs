bf9889e56fccfdaaa7a4bf49d23ac1b9338d2f0c
Andrew Dobis <andrew.dobis@epfl.ch>
2023-08-11 17:05:12 +0200

    Fixed mulitple requests and created request queue

--- src/cs/utils/model/ModelController.cs
+++ src/cs/utils/model/ModelController.cs
@@ -33,31 +33,31 @@ public partial class ModelController : Node {
 	// Model URL Constants
 	private const string MODEL_BASE_URL = "https://toby.euler.usi.ch";
 	private const string RES_FILE = "res.php";
-    private const string BAL_FILE = "bal.php";
-    private const string INSERT_METHOD = "mth=insert";
+	private const string BAL_FILE = "bal.php";
+	private const string INSERT_METHOD = "mth=insert";
 	private const string UPDATE_METHOD = "mth=update";
-    private const string DISP_METHOD = "mth=disp";
+	private const string DISP_METHOD = "mth=disp";
 	private const string RES_ID = "res_id";
 	private const string RES_NAME = "res_name";
 	private const string RES_V1 = "res_v1";
 	private const string RES_V2 = "res_v2";
-    private const string N = "n"; // The current week we are requesting
-    private const string SEASON = "s"; // The season related to the data (s < 0.5 -> WINTER, s >= 0.5 -> SUMMER)
-    private const string A_GAS = "avl_gas";
-    private const string A_NUCLEAR = "avl_nuc";
-    private const string A_RIVER = "avl_riv";
-    private const string A_SOLAR = "avl_sol";
-    private const string A_WIND = "avl_win";
-    private const string C_GAS = "cap_ele_gas";
-    private const string C_NUCLEAR = "cap_ele_nuc";
-    private const string C_RIVER = "cap_ele_riv";
-    private const string C_SOLAR = "cap_ele_sol";
-    private const string C_WIND = "cap_ele_win";
-    private const string D_BASE = "dem_base";
+	private const string N = "n"; // The current week we are requesting
+	private const string SEASON = "s"; // The season related to the data (s < 0.5 -> WINTER, s >= 0.5 -> SUMMER)
+	private const string A_GAS = "avl_gas";
+	private const string A_NUCLEAR = "avl_nuc";
+	private const string A_RIVER = "avl_riv";
+	private const string A_SOLAR = "avl_sol";
+	private const string A_WIND = "avl_wnd";
+	private const string C_GAS = "cap_ele_gas";
+	private const string C_NUCLEAR = "cap_ele_nuc";
+	private const string C_RIVER = "cap_ele_riv";
+	private const string C_SOLAR = "cap_ele_sol";
+	private const string C_WIND = "cap_ele_wnd";
+	private const string D_BASE = "dem_base";
 
-    // Game value constants
-    private const int YEARS_PER_TURN = 3;
-    private const int WEEKS_PER_YEAR = 52;
+	// Game value constants
+	private const int YEARS_PER_TURN = 3;
+	private const int WEEKS_PER_YEAR = 52;
 
 	// Reference to the game context
 	private Context C;
@@ -100,11 +100,11 @@ public partial class ModelController : Node {
 		RequestQ = new Queue<(RequestType, List<string>)>();
 	}
 
-    // Called on every frame
+	// Called on every frame
 	// In our case, used to handle outstanding model requests
-    public override void _Process(double delta) {
+	public override void _Process(double delta) {
 		// Call the base process function
-        base._Process(delta);  
+		base._Process(delta);  
 
 		// Check the model state
 		if(State == ModelState.IDLE) {
@@ -139,22 +139,22 @@ public partial class ModelController : Node {
 				}
 			}
 		}
-    }
+	}
 
-    // ==================== Public API (For General Checks) ====================
+	// ==================== Public API (For General Checks) ====================
 
-    // Checks whether or not the model is currently free
-    // Returns true if the model can handle a new request, false otherwise
-    public bool _CheckState() => State == ModelState.IDLE;
+	// Checks whether or not the model is currently free
+	// Returns true if the model can handle a new request, false otherwise
+	public bool _CheckState() => State == ModelState.IDLE;
 
 	// ==================== Server Interaction Methods ====================
 
 	// Initializes a connection with the model by creating a new game instance
 	// Returns whether or not the request was filed
-    // This method will generate the following POST request :
-    // - URL: https://toby.euler.usi.ch/res.php?mth=insert&res_id=Content.ResId 
-    // - HEADER: Null
-    // - DATA: Null
+	// This method will generate the following POST request :
+	// - URL: https://toby.euler.usi.ch/res.php?mth=insert&res_id=Content.ResId 
+	// - HEADER: Null
+	// - DATA: Null
 	public void _InitModel() {
 		// Check that the model is free
 		if(State != ModelState.IDLE) {
@@ -171,7 +171,7 @@ public partial class ModelController : Node {
 
 		// Make sure that the connection succeeded
 		try {
-            // Wait on the request's success (initialization must be done synchronously)
+			// Wait on the request's success (initialization must be done synchronously)
 			Request.Wait();
 			HttpResponseMessage Res = Request.Result;
 
@@ -205,8 +205,8 @@ public partial class ModelController : Node {
 			// Update the name to something random
 			_UpdateModelName(GenerateName());
 		} catch (HttpRequestException e) {
-            // Reset the model state in case of a crash
-            State = ModelState.IDLE;
+			// Reset the model state in case of a crash
+			State = ModelState.IDLE;
 
 			// Log the error data from the request
 			throw new Exception(
@@ -218,17 +218,17 @@ public partial class ModelController : Node {
 
 	// Updates the name of the current game instance in the model
 	// Given a new name to use for the instance (usually randomly generated)
-    // This method will generate the following POST request :
-    // - URL: https://toby.euler.usi.ch/res.php?mth=update  
-    // - HEADER: Content-Type: application/x-www-form-urlencoded
-    // - DATA: res_id = Context.ResId & res_name = @param{new_name} &
-    //         res_v1 = 42 & res_v2 = 9001 (arbitrary meme values)
+	// This method will generate the following POST request :
+	// - URL: https://toby.euler.usi.ch/res.php?mth=update  
+	// - HEADER: Content-Type: application/x-www-form-urlencoded
+	// - DATA: res_id = Context.ResId & res_name = @param{new_name} &
+	//         res_v1 = 42 & res_v2 = 9001 (arbitrary meme values)
 	public async void _UpdateModelName(string new_name) {
 		// Check that the model is free
 		if(State != ModelState.IDLE) {
-            // Enqueue our request and come back later
-            List<string> par = new() { new_name };
-            RequestQ.Enqueue((RequestType.NAME, par));
+			// Enqueue our request and come back later
+			List<string> par = new() { new_name };
+			RequestQ.Enqueue((RequestType.NAME, par));
 			return;
 		}
 		
@@ -254,8 +254,8 @@ public partial class ModelController : Node {
 			Res.EnsureSuccessStatusCode();
 		} catch (HttpRequestException e) {
 
-            // Reset the model state in case of a crash
-            State = ModelState.IDLE;
+			// Reset the model state in case of a crash
+			State = ModelState.IDLE;
 
 			// Log the error data from the request
 			throw new Exception(
@@ -272,13 +272,13 @@ public partial class ModelController : Node {
 
 		// Retrive the name from the response and store it in the context
 		string name = (
-            from r in XmlResp.Root.Descendants("row")
-            where r.Attribute(RES_ID).Value.ToInt() == C._GetGameID()
-            select r.Attribute(RES_NAME).Value.ToString()
-        ).ElementAt(0);
+			from r in XmlResp.Root.Descendants("row")
+			where r.Attribute(RES_ID).Value.ToInt() == C._GetGameID()
+			select r.Attribute(RES_NAME).Value.ToString()
+		).ElementAt(0);
 
-        // Update the context
-        C._UpdateGameName(name);
+		// Update the context
+		C._UpdateGameName(name);
 
 		// DEBUG: Check that the id was set correctly
 		Debug.Assert(name == new_name);
@@ -288,16 +288,16 @@ public partial class ModelController : Node {
 		State = ModelState.IDLE;
 	}
 
-    // Retrieves all of the data from the model and stores it in the context in a synchronous manner
-    // Warning: This will override all of the data in the context with the data 
-    // in the model. A coherency protocol should be used to avoid any mistakes.
-    // The given current turn is used to compute the timestep in the model
-    // The given turn is in batches of 3 year so it will be converted to weeks for the model.
-    // This method will generate the following two GET requests:
-    // URL = https://toby.euler.usi.ch/bal.php
-    // GET_PARAMS_1: ?mth=disp&res_id=Context.ResId&n=${@param{current_turn}*3*52}
+	// Retrieves all of the data from the model and stores it in the context in a synchronous manner
+	// Warning: This will override all of the data in the context with the data 
+	// in the model. A coherency protocol should be used to avoid any mistakes.
+	// The given current turn is used to compute the timestep in the model
+	// The given turn is in batches of 3 year so it will be converted to weeks for the model.
+	// This method will generate the following two GET requests:
+	// URL = https://toby.euler.usi.ch/bal.php
+	// GET_PARAMS_1: ?mth=disp&res_id=Context.ResId&n=${@param{current_turn}*3*52}
 	// GET_PARAMS_2: ?mth=disp&res_id=Context.ResId&n=${@param{current_turn}*3*52+26}
-    public void _FetchModelData() {
+	public void _FetchModelData() {
 
 		// Check model availability
 		if(State != ModelState.IDLE) {
@@ -308,9 +308,9 @@ public partial class ModelController : Node {
 		// Claim the model
 		State = ModelState.PENDING;
 
-        // Create the parameters
-        string resid = GetParam(RES_ID, C._GetGameID());
-        string nWinter = GetParam(N, TurnToPeakWinter(C._GetTurn()));
+		// Create the parameters
+		string resid = GetParam(RES_ID, C._GetGameID());
+		string nWinter = GetParam(N, TurnToPeakWinter(C._GetTurn()));
 		string nSummer = GetParam(N, TurnToPeakSummer(C._GetTurn()));
 
 		try {
@@ -328,6 +328,9 @@ public partial class ModelController : Node {
 				);
 			}
 
+			// Check that the results aren't empty
+			Debug.Print(wres.Result + "\n" + sres.Result);
+
 			// Parse the received data to an XML tree
 			XDocument XmlRespW = XDocument.Parse(wres.Result);
 			XDocument XmlRespS = XDocument.Parse(sres.Result);
@@ -341,11 +344,11 @@ public partial class ModelController : Node {
 			C._UdpateModelFromServer(new_MS);
 
 			// Reset the model state in case of a crash
-            State = ModelState.IDLE;
+			State = ModelState.IDLE;
 
 		} catch(HttpRequestException e) {
 			// Reset the model state in case of a crash
-            State = ModelState.IDLE;
+			State = ModelState.IDLE;
 
 			// Log the error data from the request
 			throw new Exception(
@@ -353,18 +356,18 @@ public partial class ModelController : Node {
 				" Error: " + e.Message.ToString()
 			);
 		}
-    }
+	}
 
 	// Retrieves all of the data from the model and stores it in the context in an asynchronous manner
-    // Warning: This will override all of the data in the context with the data 
-    // in the model. A coherency protocol should be used to avoid any mistakes.
-    // The given current turn is used to compute the timestep in the model
-    // The given turn is in batches of 3 year so it will be converted to weeks for the model.
-    // This method will generate the following two GET requests:
-    // URL = https://toby.euler.usi.ch/bal.php
-    // GET_PARAMS_1: ?mth=disp&res_id=Context.ResId&n=${@param{current_turn}*3*52}
+	// Warning: This will override all of the data in the context with the data 
+	// in the model. A coherency protocol should be used to avoid any mistakes.
+	// The given current turn is used to compute the timestep in the model
+	// The given turn is in batches of 3 year so it will be converted to weeks for the model.
+	// This method will generate the following two GET requests:
+	// URL = https://toby.euler.usi.ch/bal.php
+	// GET_PARAMS_1: ?mth=disp&res_id=Context.ResId&n=${@param{current_turn}*3*52}
 	// GET_PARAMS_2: ?mth=disp&res_id=Context.ResId&n=${@param{current_turn}*3*52+26}
-    public async void _FetchModelDataAsync() {
+	public async void _FetchModelDataAsync() {
 
 		// Check model availability
 		if(State != ModelState.IDLE) {
@@ -375,9 +378,9 @@ public partial class ModelController : Node {
 		// Claim the model
 		State = ModelState.PENDING;
 
-        // Create the parameters
-        string resid = GetParam(RES_ID, C._GetGameID());
-        string nWinter = GetParam(N, TurnToPeakWinter(C._GetTurn()));
+		// Create the parameters
+		string resid = GetParam(RES_ID, C._GetGameID());
+		string nWinter = GetParam(N, TurnToPeakWinter(C._GetTurn()));
 		string nSummer = GetParam(N, TurnToPeakSummer(C._GetTurn()));
 
 		try {
@@ -398,11 +401,11 @@ public partial class ModelController : Node {
 			C._UdpateModelFromServer(new_MS);
 
 			// Reset the model state in case of a crash
-            State = ModelState.IDLE;
+			State = ModelState.IDLE;
 
 		} catch(HttpRequestException e) {
 			// Reset the model state in case of a crash
-            State = ModelState.IDLE;
+			State = ModelState.IDLE;
 
 			// Log the error data from the request
 			throw new Exception(
@@ -410,7 +413,7 @@ public partial class ModelController : Node {
 				" Error: " + e.Message.ToString()
 			);
 		}
-    }
+	}
 
 	// ==================== Internal Helper Methods ====================
 
@@ -424,87 +427,87 @@ public partial class ModelController : Node {
 		return words[rnd.Next(100) % 100] + "_" + (rnd.Next()).ToString();
 	}
 
-    // Groups the URL bits into a usable URL string
-    private string ModelURL(string file, string method, params string[] getparams) {
-        // Group the file and method into url
-        string url = MODEL_BASE_URL + "/" + file + "?" + method;
-        
-        // Concatenate all get parameters
-        foreach(string p in getparams) {
-            url = url + "&" + p;
-        }
+	// Groups the URL bits into a usable URL string
+	private string ModelURL(string file, string method, params string[] getparams) {
+		// Group the file and method into url
+		string url = MODEL_BASE_URL + "/" + file + "?" + method;
+		
+		// Concatenate all get parameters
+		foreach(string p in getparams) {
+			url = url + "&" + p;
+		}
 
-        return url;
-    }
+		return url;
+	}
 
-    // Creates a valid string for individual get parametes
-    private string GetParam(string name, string value) => name + "=" + value;
-    private string GetParam<T>(string name, T value) => name + "=" + value.ToString();
+	// Creates a valid string for individual get parametes
+	private string GetParam(string name, string value) => name + "=" + value;
+	private string GetParam<T>(string name, T value) => name + "=" + value.ToString();
 
-    // Converts a turn number into a week number: either peak winter or peak summer
-    private int TurnToPeakWinter(int turn) => turn * YEARS_PER_TURN * WEEKS_PER_YEAR;
+	// Converts a turn number into a week number: either peak winter or peak summer
+	private int TurnToPeakWinter(int turn) => turn * YEARS_PER_TURN * WEEKS_PER_YEAR;
 	private int TurnToPeakSummer(int turn) => TurnToPeakWinter(turn) + (int)(WEEKS_PER_YEAR * 0.5);
 
-    // Retrieves a float attribute from a specific xml row
-    private float GetFloatAttr(IEnumerable<XElement> row, string attr) {
+	// Retrieves a float attribute from a specific xml row
+	private float GetFloatAttr(IEnumerable<XElement> row, string attr) {
 		// Attempt the query
 		try {
-       		return (from r in row select r.Attribute(attr).Value.ToFloat()).ElementAt(0);
+	   		return (from r in row select r.Attribute(attr).Value.ToFloat()).ElementAt(0);
 
 		} catch (NullReferenceException e) {
 			// In the case where the recieved data didn't match the query, simply print it to find out what's happening
-			Debug.Print("REIVED ROW: " + row.ToArray().ToString());
+			Debug.Print("REIVED ROW: " + row.ToArray().ToString() + " for attribute: " + attr);
 			throw e;
 		}
 	} 
 
-    // Retrieves an int attribute from a specific xml row
-    private int GetIntAttr(IEnumerable<XElement> row, string attr) => 
-        (from r in row select r.Attribute(attr).Value.ToInt()).ElementAt(0);
+	// Retrieves an int attribute from a specific xml row
+	private int GetIntAttr(IEnumerable<XElement> row, string attr) => 
+		(from r in row select r.Attribute(attr).Value.ToInt()).ElementAt(0);
 
-    // Converts a model XML into a Model Struct
-    // The given xml is expected to be the response from the bal.disp() server method
-    private Model ModelFromXML(XDocument xml) {
-        // Start by extracting the row
-        IEnumerable<XElement> row = from r in xml.Root.Descendants("row")
-                  where r.Attribute(RES_ID).Value.ToInt() == C._GetGameID()
-                  select r;
+	// Converts a model XML into a Model Struct
+	// The given xml is expected to be the response from the bal.disp() server method
+	private Model ModelFromXML(XDocument xml) {
+		// Start by extracting the row
+		IEnumerable<XElement> row = from r in xml.Root.Descendants("row")
+				  where r.Attribute(RES_ID).Value.ToInt() == C._GetGameID()
+				  select r;
 
-        // Retrieve the season
-        int s_val = (from s in row select s.Attribute(SEASON).Value.ToInt()).ElementAt(0);
-        ModelSeason MS = s_val < 0.5f ? ModelSeason.WINTER : ModelSeason.SUMMER;
+		// Retrieve the season
+		float s_val = (from s in row select s.Attribute(SEASON).Value.ToFloat()).ElementAt(0);
+		ModelSeason MS = s_val < 0.5f ? ModelSeason.WINTER : ModelSeason.SUMMER;
 
-        // Retrieve the subgroups of the model and return it
-        return new Model(
-            AvailabilityFromRow(row),
-            CapacityFromRow(row),
-            DemandFromRow(row),
-            ModelCoherencyState.SHARED,
-            MS
-        );
-    }
+		// Retrieve the subgroups of the model and return it
+		return new Model(
+			AvailabilityFromRow(row),
+			CapacityFromRow(row),
+			DemandFromRow(row),
+			ModelCoherencyState.SHARED,
+			MS
+		);
+	}
 
-    // Extracts the availability columns from a given row query
-    private Availability AvailabilityFromRow(IEnumerable<XElement> row) => new Availability(
-        GetFloatAttr(row, A_GAS), 
-        GetFloatAttr(row, A_NUCLEAR), 
-        GetFloatAttr(row, A_RIVER), 
-        GetFloatAttr(row, A_SOLAR), 
-        GetFloatAttr(row, A_WIND)
-    );
+	// Extracts the availability columns from a given row query
+	private Availability AvailabilityFromRow(IEnumerable<XElement> row) => new Availability(
+		GetFloatAttr(row, A_GAS), 
+		GetFloatAttr(row, A_NUCLEAR), 
+		GetFloatAttr(row, A_RIVER), 
+		GetFloatAttr(row, A_SOLAR), 
+		GetFloatAttr(row, A_WIND)
+	);
 
-    // Extracts the availability columns from a given row query
-    private Capacity CapacityFromRow(IEnumerable<XElement> row) => new Capacity(
-        GetIntAttr(row, C_GAS), 
-        GetIntAttr(row, C_NUCLEAR), 
-        GetIntAttr(row, C_RIVER), 
-        GetIntAttr(row, C_SOLAR), 
-        GetIntAttr(row, C_WIND)
-    );
+	// Extracts the availability columns from a given row query
+	private Capacity CapacityFromRow(IEnumerable<XElement> row) => new Capacity(
+		GetIntAttr(row, C_GAS), 
+		GetIntAttr(row, C_NUCLEAR), 
+		GetIntAttr(row, C_RIVER), 
+		GetIntAttr(row, C_SOLAR), 
+		GetIntAttr(row, C_WIND)
+	);
 
-    // Extracts the demand columns from a given row query
-    private Demand DemandFromRow(IEnumerable<XElement> row) => 
-        new Demand(GetIntAttr(row, D_BASE));
+	// Extracts the demand columns from a given row query
+	private Demand DemandFromRow(IEnumerable<XElement> row) => 
+		new Demand(GetIntAttr(row, D_BASE));
 
 	// ==================== Server Interaction Methods (GODOT Client) ====================
 
@@ -523,7 +526,7 @@ public partial class ModelController : Node {
 
 		// Send the request to the model
 		HTTPC.Request(
-            ModelURL(RES_FILE, INSERT_METHOD),
+			ModelURL(RES_FILE, INSERT_METHOD),
 			null,
 			Godot.HttpClient.Method.Post
 		);
