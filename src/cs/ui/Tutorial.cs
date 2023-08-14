31876c10a7ea09c122928cec9d6f98ceb5071cc4
Saara <jonessaara@gmail.com>
2023-08-14 15:40:43 +0200

    tutorial xml in 4 lang

--- src/cs/ui/Tutorial.cs
+++ src/cs/ui/Tutorial.cs
@@ -4,31 +4,31 @@ using System;
 public partial class Tutorial : CanvasLayer
 {
 	private string[] TutoText = new string[]{
-		"Congratulations !\nYou have been designated to [b]manage the power grid[/b] and ensure a sustainable and resilient energy sector.", 
-		"You need to [b]supply[/b] enough [b]energy[/b] to your population. \nThey need it to charge their phones and play their favourite video games.",
-		"You need to [b]supply[/b] enough [b]energy[/b] to your population. \nThey need it to charge their phones and play their favourite video games.", 
-		"There are different ways you can supply more energy:",
-		"There are different ways you can supply more energy:",
+		"Congratulations ! \nYou have been designated to [b]manage the power grid[/b] and ensure a sustainable and resilient energy sector.", 
+		"You need to [b]supply[/b] enough [b]energy[/b] for your population. \nThey need it to charge their phones and play their favourite video games.",
+		"You need to [b]supply[/b] enough [b]energy[/b] for your population. \nThey need it to charge their phones and play their favourite video games.", 
+		"There are different ways to supply more energy :\n- You can import energy, but it pollutes and can be expensive.",
+		"There are different ways to supply more energy :\n- You can build new power plants, but it costs time and money.",
 	};
-	//"You can reach the demand by either: \n - Implementing different policies"
+	//There are different ways to supply more energy:\n- You can implement different policies, but your population needs to approve."
 	
 	private Button B;
 	private RichTextLabel L;
 	private int i = 0;
 	private AnimationPlayer AP;
-	private NinePatchRect R2;
-	private NinePatchRect R3;
-	private NinePatchRect R4;
+	private NinePatchRect IB;
+	private NinePatchRect IB2;
+	private NinePatchRect IB3;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		B = GetNode<Button>("Rect/ColorRect/Button");
+		B = GetNode<Button>("TutoPopUp/ColorRect/Button");
 		AP = GetNode<AnimationPlayer>("AnimationPlayer");
-		L = GetNode<RichTextLabel>("Rect/ColorRect/Text");
-		R2 = GetNode<NinePatchRect>("Rect2");
-		R3 = GetNode<NinePatchRect>("Rect3");
-		R4 = GetNode<NinePatchRect>("Rect4");
+		L = GetNode<RichTextLabel>("TutoPopUp/ColorRect/Text");
+		IB = GetNode<NinePatchRect>("InfoBubble");
+		IB2 = GetNode<NinePatchRect>("InfoBubble2");
+		IB3 = GetNode<NinePatchRect>("InfoBubble3");
 		
 		L.Text = TutoText[i];
 		
@@ -41,15 +41,15 @@ public partial class Tutorial : CanvasLayer
 			i++;
 			L.Text = TutoText[i];
 			if (i == 2) {
-				R2.Show();
+				IB.Show();
 			}
 			if (i == 3) {
-				R2.Hide();
-				R3.Show();
+				IB.Hide();
+				IB2.Show();
 			}
 			if (i == 4) {
-				R3.Hide();
-				R4.Show();
+				IB2.Hide();
+				IB3.Show();
 			}
 		} else {
 			Hide();
