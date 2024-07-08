a7496937fd343e55366bb83d3bff354361742bb2
Saara <jonessaara@gmail.com>
2024-07-08 17:26:43 +0200

    context for toby

--- src/gdscript/building/BuildButton.gd
+++ src/gdscript/building/BuildButton.gd
@@ -3,15 +3,18 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	# for each last button in powerplants in buildmenu, connect to pp_pressed
 	for pp in $BuildMenu/Container.get_children():
 		pp.get_child(-1).pressed.connect(_on_pp_pressed.bind(pp))
 
 
-
+# when pressing on a buildbutton
 func _on_pressed():
 	$BuildMenu.show()
 	$BuildMenu/AnimationPlayer.play("SlideUp")
 	
+# when pressing on a powerplant in buildmenu
 func _on_pp_pressed(pp):
 	$BuildMenu.hide()
 	for plant in get_children():
